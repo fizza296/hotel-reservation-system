33d85a672fcdf00dac37a8f9fc62e7c774d5d6ff
Ahmad <ahmadu226@gmail.com>
2024-11-23 16:08:29 +0500

    Credentials update

---
+++ db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+// Create a connection to the AWS RDS MySQL database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ', err.stack);
+    return;
+  }
+  console.log('Connected to the database with thread ID: ' + connection.threadId);
+});
+
+
+
+module.exports = connection;
